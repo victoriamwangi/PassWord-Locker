f0980e2aa3a6a7d6a2663fe138790c3e1dae2f78
victoria mwangi <victoriamwangi021@gmail.com>
2022-04-24 20:20:16 +0300

    feat: test object initialization

---
+++ credentials_test.py
@@ -0,0 +1,22 @@
+import unittest
+from credentials import Credential
+
+
+class TestCredential(unittest.TestCase):
+
+    def tearDown(self):
+        Credential.accountsList = []
+
+    def setUp(self):
+        self.newCredential = Credential("twitter", "0000")
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.newCredential.accountName, "twitter")
+        self.assertEqual(self.newCredential.password, "0000")
+
+
+if __name__ == "__main__":
+    unittest.main()
